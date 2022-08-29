b43897045bc0865a687f14d8562c682462528ab6
MellyMel Melnyk <112326645+mellymelappdev4ever@users.noreply.github.com>
2022-08-29 11:34:11 +0530

    Update streamlit_app.py
    Anther Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 kale, Spincah & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Tosat')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
